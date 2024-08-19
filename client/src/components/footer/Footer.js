439a67bafe16a5d47525928a31ec8f96d268a351
unknown <aaron.hickman201@gmail.com>
2024-08-19 19:06:48 +0100

    spacing changes

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,48 +1,71 @@
 import "./footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer-container text-white p-2">
-          <div className="container">
-            <div className="row text-center">
-                <div className="col-12 col-md-4 d-none d-md-block">
-                    <div className="d-flex flex-column">
-                        <p className="fw-bold">Quick Links</p>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item bg-transparent text-white">About Us</li>
-                            <li className="list-group-item bg-transparent text-white">Services</li>
-                            <li className="list-group-item bg-transparent text-white">Testimonials</li>
-                            <li className="list-group-item bg-transparent text-white">Contact</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-12 col-md-4 d-none d-md-block">
-                    <div className="d-flex flex-column">
-                        <p className="fw-bold">Services</p>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item bg-transparent text-white">Orthotics</li>
-                            <li className="list-group-item bg-transparent text-white">Chiropractic</li>
-                            <li className="list-group-item bg-transparent text-white">Sports Remedial</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-12 col-md-4">
-                    <div className="d-flex flex-column">
-                        <p className="fw-bold">Contact</p>
-                        <div>
-                            <p>
-                                Chiropractic, 264 Alcester Rd South, <br /> 
-                                King's Heath, <br />
-                                Birmingham B14 6DR
-                            </p>
-                            <a className="text-white" href="tel:07898903772">07898 903772</a> | <a className="text-white" href="mail:info@birminghamchiropractic.uk.com">info@birminghamchiropractic.uk.com</a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className="footer-container text-white p-5 mt-5">
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-12 col-md-4 d-none d-md-block">
+            <div className="d-flex flex-column">
+              <p className="fw-bold">Quick Links</p>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item bg-transparent text-white">
+                  About Us
+                </li>
+                <li className="list-group-item bg-transparent text-white">
+                  Services
+                </li>
+                <li className="list-group-item bg-transparent text-white">
+                  Testimonials
+                </li>
+                <li className="list-group-item bg-transparent text-white">
+                  Contact
+                </li>
+              </ul>
             </div>
+          </div>
+          <div className="col-12 col-md-4 d-none d-md-block">
+            <div className="d-flex flex-column">
+              <p className="fw-bold">Services</p>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item bg-transparent text-white">
+                  Orthotics
+                </li>
+                <li className="list-group-item bg-transparent text-white">
+                  Chiropractic
+                </li>
+                <li className="list-group-item bg-transparent text-white">
+                  Sports Remedial
+                </li>
+              </ul>
             </div>
-        </footer>
-    );
+          </div>
+          <div className="col-12 col-md-4">
+            <div className="d-flex flex-column">
+              <p className="fw-bold">Contact</p>
+              <div>
+                <p>
+                  Chiropractic, 264 Alcester Rd South, <br />
+                  King's Heath, <br />
+                  Birmingham B14 6DR
+                </p>
+                <a className="text-white" href="tel:07898903772">
+                  07898 903772
+                </a>{" "}
+                |{" "}
+                <a
+                  className="text-white"
+                  href="mail:info@birminghamchiropractic.uk.com"
+                >
+                  info@birminghamchiropractic.uk.com
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
