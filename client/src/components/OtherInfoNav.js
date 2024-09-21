8d9464f58d453e69a3b7254c94c36c7ae845d9a0
unknown <aaron.hickman201@gmail.com>
2024-09-21 14:22:29 +0100

    image changes and header

--- client/src/components/OtherInfoNav.js
+++ client/src/components/OtherInfoNav.js
@@ -2,67 +2,96 @@ import team from "../images/team.jpg";
 import contact from "../images/phone.jpg";
 import testimonials from "../images/new-imgs/thumbUp.png";
 import LinkCard from "./LinkCard";
+import { useState } from "react";
 
 function OtherInfoNav() {
-	// TODO: flesh out alt text
-	const cards = [
-		{ image: team, altText: "Doctor", text: "Meet the Team", key: "meetTeam" },
-		{
-			image: testimonials,
-			altText: "Testimonials",
-			text: "Testimonials",
-			key: "Testimonials",
-		},
-		{
-			image: contact,
-			altText: "Contact Us",
-			text: "Contact Us",
-			key: "contact",
-		},
-	];
+  const [currentImage, setCurrentImage] = useState(team);
 
-	return (
-		// TODO: Make all images the same size by editing the images themselves
-		<div className="container-md my-5 pt-5">
-			<div className="row gy-3 mb-5">
-				{cards.map((it) => (
-					<div className="col-12 col-md-4" key={it.key}>
-						<LinkCard image={it.image} text={it.text} altText={it.altText}></LinkCard>
-					</div>
-				))}
-			</div>
-		</div>
+  const handleHover = (image) => {
+    setCurrentImage(image);
+  };
+  // TODO: flesh out alt text
+  const cards = [
+    { image: team, altText: "Doctor", text: "Meet the Team", key: "meetTeam" },
+    {
+      image: testimonials,
+      altText: "Testimonials",
+      text: "Testimonials",
+      key: "Testimonials",
+    },
+    {
+      image: contact,
+      altText: "Contact Us",
+      text: "Contact Us",
+      key: "contact",
+    },
+  ];
 
-		// <div className="row links">
-		//     <div className="col d-flex flex-column align-items-center justify-content-center">
-		//         <div
-		//             className="homepage-links"
-		//             src={team}
-		//             style={{ backgroundImage: `url(${team})` }}
-		//             alt="Doctor"
-		//         ></div>
-		//         <p className="link-text">Meet the team</p>
-		//     </div>
-		//     <div className="col d-flex flex-column align-items-center justify-content-center">
-		//         <div
-		//             className="homepage-links"
-		//             src={testimonials}
-		//             style={{ backgroundImage: `url(${testimonials})` }}
-		//             alt="Doctor"
-		//         ></div>
-		//         <p className="link-text">Testimonials</p>
-		//     </div>
-		//     <div className="col d-flex flex-column align-items-center justify-content-center">
-		//         <div
-		//             className="homepage-links"
-		//             src={contact}
-		//             style={{ backgroundImage: `url(${contact})` }}
-		//             alt="Doctor"
-		//         ></div>
-		//         <p className="link-text">Contact us</p>
-		//     </div>
-		// </div>
-	);
+  // return (
+  //   <div className="container-md my-5 px-0 py-5">
+  //     <div className="row">
+  //       <div className="col-md-6 col-sm-12 p-0 other-info-img">
+  //         <img className="h-100 w-100 mx-3 shadow-lg" src={currentImage} />
+  //       </div>
+  //       <div className="col-md-6 ps-5 col-sm-12">
+  //         <div
+  //           className="other-info-containers mb-4 ps-3"
+  //           onMouseEnter={() => handleHover(team)}
+  //         >
+  //           <div className="fs-1 fw-bold text-dark josefinSans-text">
+  //             Meet the Team
+  //           </div>
+  //           <p className="fs-4">
+  //             Get to know our experienced and friendly team of chiropractors who
+  //             are dedicated to providing you with personalized care.
+  //           </p>
+  //         </div>
+  //         <div
+  //           className="other-info-containers mb-4 ps-3"
+  //           onMouseEnter={() => handleHover(contact)}
+  //         >
+  //           <div className="fs-1 text-dark fw-bold josefinSans-text">
+  //             Contact Us
+  //           </div>
+  //           <p className="fs-4">
+  //             Have questions or ready to schedule an appointment? Contact us
+  //             today and our team will assist you promptly.
+  //           </p>
+  //         </div>
+  //         <div
+  //           className="other-info-containers ps-3"
+  //           onMouseEnter={() => handleHover(testimonials)}
+  //         >
+  //           <div className="fs-1 text-dark fw-bold josefinSans-text">
+  //             Testimonials
+  //           </div>
+  //           <p className="fs-4">
+  //             Read what our patients have to say about their experiences with
+  //             our chiropractic services and how we have helped them improve
+  //             their health.
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
+  return (
+    // TODO: Make all images the same size by editing the images themselves
+    <div className="container-md my-5 pt-5">
+      <div className="row gy-3 mb-5">
+        {cards.map((it) => (
+          <div className="col-12 col-md-4" key={it.key}>
+            <LinkCard
+              image={it.image}
+              text={it.text}
+              altText={it.altText}
+            ></LinkCard>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default OtherInfoNav;
--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -1,34 +1,50 @@
-import doc from "../../images/vecteezy_doctor-high-quality-4k-ultra-hd-hdr_30666538.jpg";
+import austin from "../../images/new-imgs/austinFull.jpg";
+import claire from "../../images/new-imgs/Claire.jpg";
+import adrian from "../../images/new-imgs/Adrian.jpg";
+import TeamCard from "../../components/TeamCard";
+
 import "./about.css";
 
 function About() {
   return (
-    <section>
-      <h1>team</h1>
-      <div className="team container">
-        <div className="row">
-          <div
-            className="doc-img col m-4"
-            src={doc}
-            style={{ backgroundImage: `url(${doc})` }}
-            alt="Doctor"
-          ></div>
-          <div
-            className="doc-img col m-4"
-            src={doc}
-            style={{ backgroundImage: `url(${doc})` }}
-            alt="Doctor"
-          ></div>
-          <div
-            className="doc-img col m-4"
-            src={doc}
-            style={{ backgroundImage: `url(${doc})` }}
-            alt="Doctor"
-          ></div>
-        </div>
+    <section className="border-top border-1 border-secondary">
+      <div className="d-flex justify-content-center mt-5 josefinSans-text">
+        <h2 className="about-header mt-5 fw-bold text-dark">Meet Our Team</h2>
+      </div>
+      <div className="my-5 py-5">
+        <TeamCard teamImg={austin}></TeamCard>
+      </div>
+      <div className="my-5 py-5">
+        <TeamCard teamImg={claire}></TeamCard>
+      </div>
+      <div className="my-5 py-5">
+        <TeamCard teamImg={adrian}></TeamCard>
       </div>
     </section>
   );
 }
 
 export default About;
+
+{
+  /* <div className="row">
+          <div
+            className="doc-img col m-4"
+            src={doc}
+            style={{ backgroundImage: `url(${doc})` }}
+            alt="Doctor"
+          ></div>
+          <div
+            className="doc-img col m-4"
+            src={doc}
+            style={{ backgroundImage: `url(${doc})` }}
+            alt="Doctor"
+          ></div>
+          <div
+            className="doc-img col m-4"
+            src={doc}
+            style={{ backgroundImage: `url(${doc})` }}
+            alt="Doctor"
+          ></div>
+        </div> */
+}
