1037a4f18f54a2068bfeda7043de665b94ed7574
bgrowcott <bgrowcott@ucb.ac.uk>
2024-11-12 17:14:35 +0000

    removed location

--- server/api/controllers/googleReviewsController.js
+++ server/api/controllers/googleReviewsController.js
@@ -11,7 +11,7 @@ const SCOPES = ["https://www.googleapis.com/auth/business.manage"];
 module.exports = {
 	getReviews: async function (req, res) {		
 		try {
-			const reviews = await axios.get("https://places.googleapis.com/v1/places/ChIJU3UaaA68cEgRBrD7gp9vg4Q?fields=reviews&key=" + process.env.API_KEY);
+			const reviews = await axios.get(`https://places.googleapis.com/v1/places/${process.env.LOCATION_ID}?fields=reviews&key=${process.env.API_KEY}`);
 			res.json(reviews.data);
 		} catch (error) {
 			console.error("Error retrieving reviews:", error);
